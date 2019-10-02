ec9f3c28030dfb83e31a99c24003b3f8573733dc
dani690e <dani690e@videndjurs.net>
2019-10-02 13:56:40 +0200

    Updates

---
+++ precache-manifest.44b9c706f31d3bdf2f5deb047d73dd04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b9626255914b39d7537072bf6d3b2e",
+    "url": "/scubaliving/index.html"
+  },
+  {
+    "revision": "d78fa502abbb06b96b47",
+    "url": "/scubaliving/static/css/main.aa639022.chunk.css"
+  },
+  {
+    "revision": "5b7e50713a343c14b130",
+    "url": "/scubaliving/static/js/2.5ab80b7b.chunk.js"
+  },
+  {
+    "revision": "d78fa502abbb06b96b47",
+    "url": "/scubaliving/static/js/main.73d27082.chunk.js"
+  },
+  {
+    "revision": "6220d69063b9e95199ca",
+    "url": "/scubaliving/static/js/runtime-main.865e7a5f.js"
+  },
+  {
+    "revision": "e071d9474af04abc95abe621ddcb6daf",
+    "url": "/scubaliving/static/media/logo.e071d947.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/scubaliving/precache-manifest.44b9c706f31d3bdf2f5deb047d73dd04.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scubaliving/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
